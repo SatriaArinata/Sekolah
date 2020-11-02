47fffbf8548c8cf8c531c35049235855bd258bf8
Satria Arinata <73834266+SatriaArinata@users.noreply.github.com>
2020-11-02 22:47:25 +0700

    Sekolah.py
    ini secrip nya

---
+++ Sekolah.py
@@ -0,0 +1,385 @@
+import os
+os.system("clear")
+
+import time
+
+
+
+print("              77777777777777777")
+time.sleep(0.2)
+print("            777 77 77777777777777")
+time.sleep(0.2)
+print("            777   777777777777777 ")
+time.sleep(0.2)
+print("            777777777777777777777 ")
+time.sleep(0.2)
+print("                      77777777777 ")
+time.sleep(0.2)
+print("     7777777777777777777777777777 ,,,,,,")
+time.sleep(0.2)
+print("   777777777777777777777777777777 ,,,,,,,,.")
+time.sleep(0.2)
+print("  7777777777777777777777777777777 ,,,,,,,,,. ")
+time.sleep(0.2)
+print("  7777777777777777777777777777777 ,,,,,,,,,, ")
+time.sleep(0.2)
+print("  777777777777777777777777777777  ,,,,,,,,,,  ")
+time.sleep(0.2)
+print("  77777777777777                 ,,,,,,,,,,,, ")
+time.sleep(0.2)
+print("  77777777777  .,,,,,,,,,,,,,,,,,,,,,,,,,,,,, ")
+time.sleep(0.2)
+print("  7777777777  ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,  ")
+time.sleep(0.2)
+print("  7777777777 ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,. ")
+time.sleep(0.2)
+print("   777777777 ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,.   ")
+time.sleep(0.2)
+print("     7777777 ,,,,,,,,,,,,,,,,,,,,,,,,,,,,      ")
+time.sleep(0.2)
+print("             ,,,,,,,,,,,                   ")
+time.sleep(0.2)
+print("             ,,,,,,,,,,,,,,,,,,,,,           ")
+time.sleep(0.2)
+print("             ,,,,,,,,,,,,,,,   ,,,       ")
+time.sleep(0.2)
+print("             .,,,,,,,,,,,,,, ,, ,,    ")
+time.sleep(0.2) 
+print("               ,,,,,,,,,,,,,,,,,,")
+time.sleep(2)
+print("Coding Python2")
+print
+print
+time.sleep(4)
+print("=====Tool Satria Arinata=====")
+print
+time.sleep(1)
+print("_________________________")
+time.sleep(1)
+print("Author:Satria Arinata")
+time.sleep(1)
+print("Selamat Datang ke Tool Kami")
+time.sleep(1)
+print("_________________________")
+time.sleep(3)
+print("Selamat Menjalankan Tool")
+time.sleep(3)
+print("Tool lagi masa perkembangan")
+time.sleep(3)
+print("mohon maaf kalok ada keeroran")
+time.sleep(1)
+print("=========================")
+time.sleep(1)
+print("Pilihan")
+time.sleep(1)
+print("1.Kalkulator")
+time.sleep(1)
+print("2.Menghitung luas dan keliling Lingkaran")
+time.sleep(1)
+print("3.Konversi Suhu")
+time.sleep(1)
+print("4.Proses...")
+time.sleep(1)
+print("=========================")
+time.sleep(1)
+Pilihan = input("Masukan Pilihan Anda (1/2/3/): ")
+
+if Pilihan == 1:
+   print("==========Kalkulator==========")
+   print
+   print("Pilihan")
+   time.sleep(0.2)
+   print("1.Penjumlahan")
+   time.sleep(0.2)
+   print("=========================")
+   time.sleep(0.2)
+   print("2.Pengurangan")
+   time.sleep(0.2)
+   print("=========================")
+   time.sleep(0.2)
+   print("3.Perkalian")
+   time.sleep(0.2)
+   print("=========================")
+   time.sleep(0.2)
+   print("4.Pemangkatan")
+   time.sleep(0.2)
+   print("=========================")
+   time.sleep(0.2)
+   print("5.Pembagian")
+   time.sleep(0.2)
+   print("=========================")
+   time.sleep(0.2)
+   print("6.Sisa Bagi")
+   time.sleep(0.2)
+   print("=========================")
+   A = input("Masukan Pilihan Anda (1/2/3/4/5/6): ")
+   if A == 1:
+     C = input("Masukan Bilangan Pertama: ")
+     D = input("Masukan Bilangan Kedua: ")
+     time.sleep(1)
+     print("Proses bro...")
+     hasil = C + D
+     time.sleep(4)
+     print "Jawabannya",hasil
+     
+     time.sleep(1)
+     print("Sampai Jumpa")
+     time.sleep(1)
+     print("15:Ya")
+     time.sleep(1)
+     print("20:No")
+     time.sleep(1)
+     N = input("Mau lagi (15/20): ")
+     
+     if N == 15:
+       import os
+       os.system("python2 Wawa.py")
+       
+     elif N == 20:
+       time.sleep(2)
+       print("Keluar...")
+       import os
+       os.system("exit")
+     
+    
+     
+   
+   elif A == 2:
+     C = input("Masukan Bilangan Pertama: ")
+     D = input("Masukan Bilangan Kedua: ")
+     time.sleep(1)
+     print("Proses bro...")
+     hasil = C - D
+     time.sleep(4)
+     print "Jawabannya",hasil
+     time.sleep(1)
+     print("Sampai Jumpa")
+     time.sleep(1)
+     print("15:Ya")
+     time.sleep(1)
+     print("20:No")
+     time.sleep(1)
+     N = input("Mau lagi (15/20): ")
+     
+     if N == 15:
+       import os
+       os.system("python2 Wawa.py")
+       
+     elif N == 20:
+       time.sleep(2)
+       print("Keluar...")
+       import os
+       os.system("exit") 
+   
+   elif A == 3:
+     C = input("Masukan Bilangan Pertama: ")
+     D = input("Masukan Bilangan Kedua: ")
+     time.sleep(1)
+     print("Proses bro...")
+     hasil = C * D
+     time.sleep(4)
+     print "Jawabannya",hasil
+     time.sleep(1)
+     print("Sampai Jumpa")
+     time.sleep(1)
+     print("15:Ya")
+     time.sleep(1)
+     print("20:No")
+     time.sleep(1)
+     N = input("Mau lagi (15/20): ")
+     
+     if N == 15:
+       import os
+       os.system("python2 Wawa.py")
+       
+     elif N == 20:
+       time.sleep(2)
+       print("Keluar...")
+       import os
+       os.system("exit")
+   
+   elif A == 4:
+     C = input("Masukan Bilangan Pertama: ")
+     D = input("Masukan Bilangan Kedua: ")
+     time.sleep(1)
+     print("Proses bro...")
+     hasil = C ** D
+     time.sleep(4)
+     print "Jawabannya =",hasil
+     time.sleep(1)
+     print("Sampai Jumpa")
+     time.sleep(1)
+     print("15:Ya")
+     time.sleep(1)
+     print("20:No")
+     time.sleep(1)
+     N = input("Mau lagi (15/20): ")
+     
+     if N == 15:
+       import os
+       os.system("python2 Wawa.py")
+       
+     elif N == 20:
+       time.sleep(2)
+       print("Keluar...")
+       import os
+       os.system("exit")
+   
+   elif A == 5:
+     C = input("Masukan Bilangan Pertama: ")
+     D = input("Masukan Bilangan Kedua: ")
+     time.sleep(1)
+     print("Proses bro...")    
+     hasil = C / D
+     time.sleep(4)
+     print "Jawabannya =",hasil
+     time.sleep(1)
+     print("Sampai Jumpa")
+     time.sleep(1)
+     print("15:Ya")
+     time.sleep(1)
+     print("20:No")
+     time.sleep(1)
+     N = input("Mau lagi (15/20): ")
+     
+     if N == 15:
+       import os
+       os.system("python2 Wawa.py")
+       
+     elif N == 20:
+       time.sleep(2)
+       print("Keluar...")
+       import os
+       os.system("exit")
+   
+   elif A == 6:
+     C = input("Masukan Bilangan Pertama: ")
+     D = input("Masukan Bilangan Kedua: ")
+     time.sleep(1)
+     print("Proses bro...")
+     hasil = C % D
+     time.sleep(4)
+     print "Jawabannya =",hasil
+     time.sleep(1)
+     print("Sampai Jumpa")
+     time.sleep(1)
+     print("15:Ya")
+     time.sleep(1)
+     print("20:No")
+     time.sleep(1)
+     N = input("Mau lagi (15/20): ")
+     
+     if N == 15:
+       import os
+       os.system("python2 Wawa.py")
+       
+     elif N == 20:
+       time.sleep(2)
+       print("Keluar...")
+       import os
+       os.system("exit")
+   
+   else:
+     print("Input yang anda masukan salah")
+     time.sleep(1)
+     print("15:Ya")
+     time.sleep(1)
+     print("20:No")
+     time.sleep(1)
+     N = input("Mau lagi (15/20): ")
+     
+     if N == 15:
+       import os
+       os.system("python2 Wawa.py")
+       
+     elif N == 20:
+       time.sleep(2)
+       print("Keluar...")
+       import os
+       os.system("exit")
+     
+elif Pilihan == 2:
+  time.sleep(1)
+  print("Loading...")
+  phi = 3.14
+  time.sleep(4)
+  r = input("Masukkan panjang jari-jari lingkaran: ")
+  k = 2 * phi * r
+  time.sleep(1)
+  print("Proses bro...")
+  time.sleep(4)
+  print "Keliling Lingkaran adalah =",k
+  time.sleep(1)
+  print("Sampai Jumpa")
+  time.sleep(1)
+  print("15:Ya")
+  time.sleep(1)
+  print("20:No")
+  time.sleep(1)
+  N = input("Mau lagi (15/20): ")
+     
+  if N == 15:
+    import os
+    os.system("python2 Wawa.py")
+       
+  elif N == 20:
+    time.sleep(2)
+    print("Keluar...")
+    import os
+    os.system("exit")
+     
+elif Pilihan == 3:
+  time.sleep(1)
+  print ('Konversi Suhu Dari Celcius Ke Fahrenheit Ke Reamur dan Kelvin')
+  time.sleep(4)
+  suhu_celcius =float(input('Masukan Suhu Dalam Celcius: '))
+  suhu_fahrenheit = (9./5)* suhu_celcius + 32
+  suhu_reamur = (4./5)* suhu_celcius
+  suhu_kelvin = suhu_celcius + 273.15
+  time.sleep(1)
+  print ('Hasil Perhitungan Konversi Suhu: ')
+  time.sleep(1)
+  print ('Suhu Celcius adalah    : %f' %(suhu_celcius))
+  time.sleep(1)
+  print ('Suhu Fahrenheit adalah : %2f' %(suhu_fahrenheit))
+  time.sleep(1)
+  print ('Suhu Reamur adalah     : %f' %(suhu_reamur))
+  time.sleep(1)
+  print ('Suhu Kelvin adalah     : %f' % (suhu_kelvin))
+  time.sleep(1)
+  print("Sampai Jumpa")
+  time.sleep(1)
+  print("15:Ya")
+  time.sleep(1)
+  print("20:No")
+  time.sleep(1)
+  N = input("Mau lagi (15/20): ")
+     
+  if N == 15:
+    import os
+    os.system("python2 Wawa.py")
+       
+  elif N == 20:
+    time.sleep(2)
+    print("Keluar...")
+    time.sleep(3)
+    import os
+    os.system("exit")
+  
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
